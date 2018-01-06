d9c55626ec471eb5a9ba856c8a0c1007f016ade8
Dominik Kotecki <dominikus1910@gmail.com>
2018-01-06 18:44:50 +0100

    sentiment service init

--- src/SentimentFS.SentimentService/SentimentFS.SentimentService.fs
+++ src/SentimentFS.SentimentService/SentimentFS.SentimentService.fs
@@ -2,6 +2,10 @@ namespace SentimentFS.SentimentService
 
 open Akkling
 open System
+open System.Net.Http
+open Newtonsoft.Json
+open System.Collections.Generic
+open System.Text
 
 module Messages =
     open SentimentFS.NaiveBayes.Dto
@@ -14,30 +18,49 @@ module Messages =
         | VeryPositive = 2
 
     [<CLIMutable>]
-    type Classify = { text : string }
+    type TrainRequest = { text: string; category: Emotion; weight : int }
 
-    [<CLIMutable>]
-    type Train = { trainQuery : TrainingQuery<Emotion> }
 
-    type SentimentActorCommand =
-        | Train of Train
-        | Classify of Classify
-
-    type SentimentMessage =
-        | TrainEvent of Train
-        | SentimentCommand of SentimentActorCommand
+    type SentimentServiceMessages = 
+        | Train of TrainRequest * string
 
 module Program =
+    open Messages
     open Akka.Actor
+    
+    let private intToEmotion (value: int): Emotion =
+        match value with
+        | -5 | -4 -> Emotion.VeryNegative
+        | -3 | -2 | -1 -> Emotion.Negative
+        | 0 -> Emotion.Neutral
+        | 1 | 2 | 3 -> Emotion.Positive
+        | 4 | 5 -> Emotion.VeryPositive
+        | _ -> Emotion.Neutral
+
+    let httpClient = HttpClient();
+
+    let actor _ = function
+        | Train (req, url) ->
+            use content = new StringContent((req |> JsonConvert.SerializeObject), UTF8Encoding.UTF8, "application/json")
+            async {
+                return! httpClient.PutAsync(url, content) |> Async.AwaitTask
+            } |> Async.RunSynchronously |> ignored
+
+    let initClassifier apiUrl tainingDataUrl (actor: IActorRef<SentimentServiceMessages>) =
+        let httpResult = async {
+                let! result = httpClient.GetAsync(System.Uri(tainingDataUrl)) |> Async.AwaitTask
+                result.EnsureSuccessStatusCode() |> ignore
+                return! result.Content.ReadAsStringAsync() |> Async.AwaitTask } |> Async.RunSynchronously
+
+        let emotions = httpResult |> JsonConvert.DeserializeObject<IDictionary<string, int>>
+
+        for keyValue in emotions do
+            actor <! (Train(({ text = keyValue.Key; category = keyValue.Value |> intToEmotion; weight = 1 } , apiUrl)))
 
     [<EntryPoint>]
     let main argv =
         let system = System.create "sentimentfs" <| Configuration.load()
-        let remoteProps addr actor = { propsPersist actor with Deploy = Some (Deploy(RemoteScope(Address.Parse addr)));}
-        let actor = spawn system "sentiment" <| (remoteProps "akka.tcp://sentimentfs@localhost:4500" (sentimentActor(Some defaultClassificatorConfig)))
-        // actor <! SentimentCommand(Train({ trainQuery = { value = "I love fsharp"; category = Emotion.Positive; weight = None } }))
-        // actor <! SentimentCommand(Train({ trainQuery = { value = "I hate java"; category = Emotion.Negative; weight = None } }))
-        // async { let! reply = actor <? SentimentCommand(Classify({ text = "My brother love fsharp" }))
-        //         printfn "Current state of %A: %A" actor reply } |> Async.RunSynchronously
+        let actor = spawn system "sentiment" <| props (actorOf2 (actor))
+        initClassifier "http://localhost:5000/api/sentiment/trainer" "https://raw.githubusercontent.com/wooorm/afinn-96/master/index.json" actor
         Console.ReadKey();
         0 // return an integer exit code
